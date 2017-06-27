7c8fc96fc872ed282f6264d71da7c29839402120
Alexander Prooks <aprooks@live.ru>
2017-06-27 22:28:10 +0200

    feat(Auth0): working lock.Show()
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

--- src/Client/pages/LoginAuth.fs
+++ src/Client/pages/LoginAuth.fs
@@ -17,22 +17,10 @@ let myConfig =
 
 
 
-// type IAuth0Lock = 
-//     abstract SubProto: JsConstructor<obj>
-// let a = IAuth0Lock.SubProto.Create(null)
+let auth0lock:JsConstructor<string,string,obj> = importDefault "auth0-lock/lib/index.js"
 
-// let auth0Js = importAll<obj> "auth0-js"
-let auth0lock:obj = importDefault "auth0-lock/lib/index.js"
-let a = (createNew auth0lock) $("","")
-// let constr  = createNew lock
-// let l = createNew lock ("fable-test.eu.auth0.com","E0a7096d6yYIPYtVdDR1XEChjqsA7p4e")
+let authInstance = auth0lock.Create("E0a7096d6yYIPYtVdDR1XEChjqsA7p4e","fable-test.eu.auth0.com")
 
-
-// let getAuth0instance = createNew ( lock )
-// let instance = getAuth0instance ("fable-test.eu.auth0.com","E0a7096d6yYIPYtVdDR1XEChjqsA7p4e")
 let login() =
-    console.log "huy"
     printfn "%O" auth0lock
-    // printfn "%O" auth0Js
-    // printfn "%O" constr?version
-    // instance?show()
+    authInstance?show()
