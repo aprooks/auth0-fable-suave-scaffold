8da5707912c91daa243847e79c6f103d55943e32
Alexander Prooks <aprooks@live.ru>
2017-06-29 23:11:32 +0200

    refactor: callback helper
    Signed-off-by: Alexander Prooks <aprooks@live.ru>

--- src/Client/pages/LoginAuth.fs
+++ src/Client/pages/LoginAuth.fs
@@ -23,18 +23,6 @@ type Model =
 let auth0lock:JsConstructor<string,string,obj> = importDefault "auth0-lock/lib/index.js"
 
 let lock = auth0lock.Create("E0a7096d6yYIPYtVdDR1XEChjqsA7p4e","fable-test.eu.auth0.com")
-debugger()
-// lock?on("authenticated",
-//     fun authResult ->
-//        debugger()
-//        printfn "%O" authResult
-//        Utils.save "auth" authResult
-// ) |> ignore
-
-lock?on("show",
-    fun _ ->
-        printfn "show"
-    ) |> ignore
 
 let isAccessToken() = 
     let hash = window.location.hash
@@ -42,29 +30,46 @@ let isAccessToken() =
     | true -> Some hash
     | false -> None
 
-let tokenurl = """http://localhost:8080/#access_token=fpngg5V9VCKmfhle&expires_in=86400&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2ZhYmxlLXRlc3QuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU5NTIyOWMzYTNlNDM3NmZlYmQ0MDBjZSIsImF1ZCI6IkUwYTcwOTZkNnlZSVBZdFZkRFIxWEVDaGpxc0E3cDRlIiwiZXhwIjoxNDk4NzAwODk5LCJpYXQiOjE0OTg2NjQ4OTl9.Fo4L-xe8IEwJCtM8YTpx5XUYKxDRPRPpltYtxTfs3Qs&token_type=Bearer&state=za51ObdDLsuxTTR50UnzITuQFrRuRZVN"""
+let exampleTokenUrl = """http://localhost:8080/#access_token=fpngg5V9VCKmfhle&expires_in=86400&id_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL2ZhYmxlLXRlc3QuZXUuYXV0aDAuY29tLyIsInN1YiI6ImF1dGgwfDU5NTIyOWMzYTNlNDM3NmZlYmQ0MDBjZSIsImF1ZCI6IkUwYTcwOTZkNnlZSVBZdFZkRFIxWEVDaGpxc0E3cDRlIiwiZXhwIjoxNDk4NzAwODk5LCJpYXQiOjE0OTg2NjQ4OTl9.Fo4L-xe8IEwJCtM8YTpx5XUYKxDRPRPpltYtxTfs3Qs&token_type=Bearer&state=za51ObdDLsuxTTR50UnzITuQFrRuRZVN"""
 
 open Messages
 open Fable.Helpers.React
 open Fable.Helpers.React.Props
 
+let callback cb =
+    System.Func<obj,obj,unit>(
+        fun err result -> 
+        if not <| isNull err then 
+            Result.Error err |> cb
+        else if not <| isNull result then
+            Result.Ok result |> cb
+        else
+            failwith "Both error and result are empty which should be impossible ¯\\_(ツ)_/¯"
+    )
+    
+
 let view (model:Model) (dispatch: AppMsg -> unit) = 
     match model with 
     | TokenValidation token ->
-        lock?resumeAuth(token,System.Func<obj,obj,unit>( fun err result->
-            if not <| isNull err then 
-                printfn "Error on resumeAuth: %O" err
-            
-            printfn "Token received: %O" result
-
-            lock?getUserInfo(result?accessToken, 
-                System.Func<obj,obj,unit> (fun err result ->
-                    if not<| isNull result then
-                        printfn "userProfile loaded %O" result
-                    AppMsg.LoggedIn |> dispatch
-                )
-            )|>ignore
-        )) |> ignore
+        lock?resumeAuth(
+            token,
+            callback <| fun res -> 
+                match res with
+                | Result.Error err ->
+                    printfn "Error on resumeAuth: %O" err
+                | Result.Ok authResult -> 
+                    printfn "Token received: %O" authResult
+        
+                    lock?getUserInfo(authResult?accessToken, 
+                        callback <| fun res -> 
+                            match res with
+                            | Result.Ok profile ->
+                                printfn "userProfile loaded %O" profile
+                                AppMsg.LoggedIn |> dispatch
+                            | Result.Error err -> 
+                                printfn "error getting user profile %O" err
+                        )|> ignore
+        ) |> ignore
         
         div [Id "loginAuth view"] [str "Login auth0"]
     | Model.Login ->
